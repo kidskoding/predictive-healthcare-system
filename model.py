de0197265dd5ac84bf17b8300d3278ae29073a16
Anirudh Konidala <kidskodingclub@gmail.com>
2024-12-24 01:22:49 -0600

    Updated ML model to be class based

--- model.py
+++ model.py
@@ -1,39 +1,57 @@
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 import joblib
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-def load_data(dataset_path, target_column):
-    data = pd.read_csv(dataset_path)
-    X = data.drop(target_column, axis=1)
-    y = data[target_column]
-    return X, y
+class Model:
+    def __init__(self, dataset_path, target_column):
+        self.dataset_path = dataset_path
+        self.target_column = target_column
+        self.model = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
 
-def preprocess_data(X):
-    X = X.fillna(X.mean())
-    return X
+    def load_data(self):
+        data = pd.read_csv(self.dataset_path)
+        X = data.drop(self.target_column, axis=1)
+        y = data[self.target_column]
+        return X, y
 
-def train_model(X_train, y_train):
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
-    return model
+    def preprocess_data(self, X):
+        X = X.fillna(X.mean())
+        return X
 
-def test_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    print("Accuracy Score:", accuracy_score(y_test, y_pred), end="\n\n")
-    print("Classification Report:\n", classification_report(y_test, y_pred))
+    def train_test_split_data(self, X, y):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-def save_model(model):
-    filename = "./model.joblib"
-    joblib.dump(model, filename)
-    print(f"Model saved to {filename}")
+    def train_model(self):
+        self.model = RandomForestClassifier(random_state=42)
+        self.model.fit(self.X_train, self.y_train)
 
-def visualize_class_distribution(y):
-    plt.figure(figsize=(8, 6))
-    sns.countplot(x=y)
-    plt.title('Class Distribution')
-    plt.show()
+    def test_model(self):
+        if self.model is None:
+            print("Model not trained yet!")
+            return
+
+        y_pred = self.model.predict(self.X_test)
+        print("Accuracy Score:", accuracy_score(self.y_test, y_pred), end="\n\n")
+        print("Classification Report:\n", classification_report(self.y_test, y_pred))
+
+    def save_model(self):
+        if self.model is None:
+            print("Model is not trained yet!")
+            return
+
+        filename = "./model.joblib"
+        joblib.dump(self.model, filename)
+        print(f"Model saved to {filename}")
+
+    def load_model(self):
+        filename = "./model.joblib"
+        self.model = joblib.load(filename)
+        print(f"Model loaded from {filename}")
